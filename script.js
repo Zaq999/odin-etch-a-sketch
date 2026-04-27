3f102d7b9cb2d9a6ea74a810b2c11531795e4e3f
zaq999 <e.madera11@gmail.com>
2026-04-27 17:39:01 +0200

    Add fucntion that creates a grid and rainbow mode

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 const container = document.querySelector("#container");
-const div = document.createElement("div");
-div.textContent = "hello!";
+
+const setBg = () => {
+  const randomColor = Math.floor(Math.random()*16777215).toString(16);
+  newColor = "#" + randomColor;
+  return newColor;
+}
+
+for (let i = 0; i < 10; i++) {
+  let column = document.createElement("div");
+  column.setAttribute(
+    "style",
+    "display: flex; flex-grow: 1; flex-wrap: wrap; flex-direction: column; ",
+  );
+  container.appendChild(column);
+
+  for (let i = 0; i < 10; i++) {
+    let row = document.createElement("div");
+    row.setAttribute(
+      "style",
+      "background: white; border: 1px solid; flex-grow: 1;",
+    );
+    column.appendChild(row);
+
+    row.addEventListener("mouseover", () => {
+     row.style.backgroundColor = setBg();
+    })
+  }
+}
 
 
-
-container.appendChild(div);
