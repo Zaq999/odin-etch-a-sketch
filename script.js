997f418b2798d699c083b9f975f2592aaea6c5b9
zaq999 <e.madera11@gmail.com>
2026-04-28 20:46:04 +0200

    Add clear and resize button functionality

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+let userGridSize = 16;
 const container = document.querySelector("#container");
+const clearBtn = document.querySelector("#clear");
+const sizeBtn = document.querySelector("#size");
+const rainbowBtn = document.querySelector("#rainbow")
+const column = document.createElement("div");
 
 const setBg = () => {
   const randomColor = Math.floor(Math.random()*16777215).toString(16);
@@ -6,16 +11,20 @@ const setBg = () => {
   return newColor;
 }
 
-for (let i = 0; i < 10; i++) {
-  let column = document.createElement("div");
+
+function createGrid () {
+for (let i = 0; i < userGridSize; i++) {
+  const column = document.createElement("div");
   column.setAttribute(
     "style",
     "display: flex; flex-grow: 1; flex-wrap: wrap; flex-direction: column; ",
   );
   container.appendChild(column);
 
-  for (let i = 0; i < 10; i++) {
-    let row = document.createElement("div");
+  
+
+  for (let i = 0; i < userGridSize; i++) {
+    const row = document.createElement("div");
     row.setAttribute(
       "style",
       "background: white; border: 1px solid; flex-grow: 1;",
@@ -24,8 +33,27 @@ for (let i = 0; i < 10; i++) {
 
     row.addEventListener("mouseover", () => {
      row.style.backgroundColor = setBg();
-    })
-  }
+    });
+
+    clearBtn.addEventListener("click", () => {
+    row.style.backgroundColor = "white";
+    });
+ 
+  
 }
+}
+return column;
+}
+
+createGrid();
+sizeBtn.addEventListener("click", () => {
+  userGridSize = prompt("Choose grid size from 1 to 100 (per row).", "1-100");
+  createGrid();
+})
+rainbowBtn.addEventListener("click", () => {
+  container.removeChild(column);
+  })
+
+
 
 
