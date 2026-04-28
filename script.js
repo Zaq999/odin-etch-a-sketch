240ba5e73df2494b53a3784e68fe2fa30cc54434
zaq999 <e.madera11@gmail.com>
2026-04-28 22:50:53 +0200

    Fix resize function

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const container = document.querySelector("#container");
 const clearBtn = document.querySelector("#clear");
 const sizeBtn = document.querySelector("#size");
 const rainbowBtn = document.querySelector("#rainbow")
-const column = document.createElement("div");
 
 const setBg = () => {
   const randomColor = Math.floor(Math.random()*16777215).toString(16);
@@ -11,49 +10,48 @@ const setBg = () => {
   return newColor;
 }
 
-
 function createGrid () {
-for (let i = 0; i < userGridSize; i++) {
-  const column = document.createElement("div");
-  column.setAttribute(
-    "style",
-    "display: flex; flex-grow: 1; flex-wrap: wrap; flex-direction: column; ",
-  );
-  container.appendChild(column);
-
-  
-
   for (let i = 0; i < userGridSize; i++) {
-    const row = document.createElement("div");
-    row.setAttribute(
+    const column = document.createElement("div");
+    column.setAttribute("id", "column")
+    column.setAttribute(
       "style",
-      "background: white; border: 1px solid; flex-grow: 1;",
+      "display: flex; flex-grow: 1; flex-wrap: wrap; flex-direction: column; ",
     );
-    column.appendChild(row);
+    container.appendChild(column);
 
-    row.addEventListener("mouseover", () => {
-     row.style.backgroundColor = setBg();
-    });
+    for (let i = 0; i < userGridSize; i++) {
+      const row = document.createElement("div");
+      row.setAttribute(
+        "style",
+        "background: white; border: 1px solid; flex-grow: 1;",
+      );
+      column.appendChild(row);
 
-    clearBtn.addEventListener("click", () => {
-    row.style.backgroundColor = "white";
-    });
- 
-  
+      row.addEventListener("mouseover", () => {
+      row.style.backgroundColor = setBg();
+      });
+
+      clearBtn.addEventListener("click", () => {
+      row.style.backgroundColor = "white";
+      });
+    }
+    
 }
 }
-return column;
-}
-
 createGrid();
 sizeBtn.addEventListener("click", () => {
+  for(let i = 0; i < userGridSize; i++) {
+    const columnPointer = document.querySelector("#column")
+    container.removeChild(columnPointer);
+  }
   userGridSize = prompt("Choose grid size from 1 to 100 (per row).", "1-100");
+  if(userGridSize > 100) {
+    userGridSize = prompt("You want you'r PC to explode? Please choose from 1 to 100");
+  }
   createGrid();
 })
 rainbowBtn.addEventListener("click", () => {
-  container.removeChild(column);
+
   })
-
-
-
 
